d03e36379093b874146bc40e74d493954e80dd35
Harish <harishpoctor@gmail.com>
2022-06-26 22:16:56 +0530

    Implemented Edit and Delete of individual project

--- MyProfileApp/Controllers/ProjectController.cs
+++ MyProfileApp/Controllers/ProjectController.cs
@@ -1,4 +1,5 @@
 ﻿using MyProfileApp.Models;
+using MyProfileApp.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,33 +22,58 @@ namespace MyProfileApp.Controllers
             _context.Dispose();
         }
 
-        [Route("Project/AddProject/{perId}")]
-        public ActionResult AddProject(int perId)
+        [Route("Project/AddEditProject/{perId}")]
+        public ActionResult AddEditProject(int perId)
         {
-            return View(new Project { PersonId = perId });
+            return View(new AddEditProjectViewModel
+            {
+                Project = new Project { PersonId = perId },
+                ExistingProjects = _context.Projects.Where(p => p.PersonId == perId).ToList()
+            });
+        }
+
+        [Route("Project/AddEditProject/{perId}/{projectId}", Name = "EditProject")]
+        public ActionResult AddEditProject(int perId, int projectId)
+        {
+            return View(new AddEditProjectViewModel
+            {
+                Project = _context.Projects.Single(p => p.Id == projectId),
+                ExistingProjects = _context.Projects.Where(p => p.PersonId == perId).ToList()
+            });
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult SaveProject(Project project)
+        public ActionResult SaveProject(AddEditProjectViewModel viewModel)
         {
             if (!ModelState.IsValid)
             {
-                return View("AddProject", project);
+                return View("AddEditProject", viewModel);
             }
-            if (project.Id == 0)
+            if (viewModel.Project.Id == 0)
             {
-                _context.Projects.Add(project);
+                _context.Projects.Add(viewModel.Project);
             }
             else
             {
-                var projectInDb = _context.Projects.Single(p => p.Id == project.Id);
-                projectInDb.Title = project.Title;
-                projectInDb.Subtitle = project.Subtitle;
-                projectInDb.Description = project.Description;
+                var projectInDb = _context.Projects.Single(p => p.Id == viewModel.Project.Id);
+                projectInDb.Title = viewModel.Project.Title;
+                projectInDb.Subtitle = viewModel.Project.Subtitle;
+                projectInDb.Description = viewModel.Project.Description;
             }
             _context.SaveChanges();
-            return RedirectToAction("AddEditInfo", "Profile", new { id = project.PersonId });
+            return RedirectToAction("AddEditInfo", "Profile", new { id = viewModel.Project.PersonId });
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var projectInDb = _context.Projects.SingleOrDefault(p => p.Id == id);
+            if (projectInDb == null)
+                return HttpNotFound();
+
+            _context.Projects.Remove(projectInDb);
+            _context.SaveChanges();
+            return RedirectToAction("AddEditProject", "Project", new { id = projectInDb.PersonId });
         }
     }
 }
---
+++ MyProfileApp/ViewModels/AddEditProjectViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MyProfileApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyProfileApp.ViewModels
+{
+    public class AddEditProjectViewModel
+    {
+        public Project Project { get; set; }
+
+        public IEnumerable<Project> ExistingProjects { get; set; }
+    }
+}
